77dfcc5cb728b7e247f8883e2c4585ffdde3ff87
Sujithkumarvelpula <113710355+Sujithkumarvelpula@users.noreply.github.com>
2022-11-16 22:42:11 +0530

    Update todo.js

--- __tests__/todo.js
+++ __tests__/todo.js
@@ -11,19 +11,19 @@ describe("Todolist Test Suite", () => {
     const oneDay = 60 * 60 * 24 * 1000;
     [
       {
-        title: "Buy Groceries",
+        title: "Buy Groceriees",
         completed: false,
         dueDate: new Date(today.getTime() - 2 * oneDay).toLocaleDateString(
           "en-CA"
         ),
       },
       {
-        title: "Pay Hosetel fee",
+        title: "Pay Hostel fee",
         completed: false,
         dueDate: new Date().toLocaleDateString("en-CA"),
       },
       {
-        title: "Write homework",
+        title: "Write homework!",
         completed: false,
         dueDate: new Date(today.getTime() + 2 * oneDay).toLocaleDateString(
           "en-CA"
@@ -31,11 +31,11 @@ describe("Todolist Test Suite", () => {
       },
     ].forEach(add);
   });
-  test("Should add a new todo", () => {
+  test("Should add a new todoo", () => {
     expect(all.length).toEqual(3);
 
     add({
-      title: "A test item",
+      title: "A test itemm",
       completed: false,
       dueDate: new Date().toLocaleDateString("en-CA"),
     });
@@ -43,21 +43,21 @@ describe("Todolist Test Suite", () => {
     expect(all.length).toEqual(4);
   });
 
-  test("Should mark a todo as complete", () => {
+  test("Should mark a todo as completed", () => {
     expect(all[0].completed).toEqual(false);
     markAsComplete(0);
     expect(all[0].completed).toEqual(true);
   });
 
-  test("Should retrieve overdue items", () => {
+  test("Should retrieve overdue items:", () => {
     expect(overdue().length).toEqual(1);
   });
 
-  test("Should retrieve due today items", () => {
+  test("Should retrieve due today items:", () => {
     expect(dueToday().length).toEqual(2);
   });
 
-  test("Should retrieve due later items", () => {
+  test("Should retrieve due  later items", () => {
     expect(dueLater().length).toEqual(1);
   });
 });
